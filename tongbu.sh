f5a26661a54f08506c4e2b172475869d61182b6b
hai <linhaier@qq.com>
2017-12-11 09:26:37 +0800

    first realtime_sync

---
+++ tongbu.sh
@@ -0,0 +1,8 @@
+/usr/bin/inotifywait -mrq  --timefmt '%d/%m/%y %H:%M:%S' --format '%T %e %w%f' -e create /var/spool/Record/2017/10/ | while read file; 
+do 
+INO_FILE=$(echo $file | awk '{print $4}')
+ echo $file | awk '{print $4}'
+
+ /usr/bin/rsync -avuz -e 'ssh -p 22'  --size-only  ${INO_FILE} root@192.168.X.X:/home/media/ > /dev/null 2>&1 &
+
+done
