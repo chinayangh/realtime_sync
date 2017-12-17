a3254e4b3920d3f72cdda35d4c3afee44974aab2
chinayangh <linhaier@qq.com>
2017-12-17 23:12:04 +0800

    Update tongbu.sh

--- tongbu.sh
+++ tongbu.sh
@@ -2,7 +2,7 @@
 /usr/bin/inotifywait -mrq  --timefmt '%d/%m/%y %H:%M:%S' --format '%T %e %w%f' -e create,close_write /var/spool/Record/2017/10/ | while read file; 
 do 
 INO_FILE=$(echo $file | awk '{print $4}')
- echo $file | awk '{print $4}'
+#echo $file | awk '{print $4}'
 
  /usr/bin/rsync -avuz -e 'ssh -p 22'  --size-only  ${INO_FILE} root@192.168.X.X:/home/media/10/ > /dev/null 2>&1 &
 
